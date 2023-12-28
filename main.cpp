cfde7a751952cdfdaad3d82f59101b4fdeb94d6c
ShkalikovDenis4m0 <denis.slinovo.27@gmail.com>
2023-12-28 09:02:46 +0300

    updat main in master

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 using namespace std;
 
+void Noch_ulica_fonar_apteka();
+
 int main()
 {
+    setlocale(LC_ALL, "Russian");
     cout<<"Hello World";
+    Noch_ulica_fonar_apteka();
     return 0;
+}
+
+void Noch_ulica_fonar_apteka()
+{
+    cout<<"Ночь, улица, фонарь, аптека,"<<
+    "\nБессмысленный и тусклый свет."<<
+    "\nЖиви еще хоть четверть века —"<<
+    "\nВсё будет так. Исхода нет."<<
+    "\nУмрёшь — начнёшь опять сначала"<<
+    "\nИ повторится всё, как встарь:"<<
+    "\nНочь, ледяная рябь канала,"<<
+    "\nАптека, улица, фонарь.\n";
 }
